ec5c2b43363d99d10ee22be80bf2672fd30d2c69
hwang <hwang@DESKTOP-R006L5M>
2025-01-29 20:37:33 +0900

    "백준 2562 레벨 브론즈3"

---
+++ Backjoon_Target_Goldgo/src/Main/b2884.java
@@ -0,0 +1,27 @@
+package Main;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b2884 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int hours = Integer.parseInt(st.nextToken());
+		int mins = Integer.parseInt(st.nextToken());
+	
+		int beforetime = (hours * 60) + mins;
+		//전체 시간에서 45분을 빼고 시, 분으로 분리
+		int aftertime = beforetime - 45;
+		if(aftertime < 0) {
+			aftertime += 24 * 60;
+		}
+		int afterhours = aftertime / 60;
+		int aftermins = aftertime - (afterhours * 60);
+		System.out.print(afterhours + " " + aftermins);
+	}
+
+}
