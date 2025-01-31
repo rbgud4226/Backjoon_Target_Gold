ade372fca0183e157b99f813b879459673f7347c
dofla <dofla@192.168.10.101>
2025-01-31 18:02:59 +0900

    "백준 8958 레벨 브론즈2"

---
+++ Backjoon_Target_Goldgo/src/Main/b8958.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b8958 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            String text = br.readLine();
+            int score = 0;
+            int count = 0;
+            for (int j = 0; j < text.length(); j++) {
+                if (text.charAt(j) == 'O') {
+                    count++;
+                    score += count;
+                } else {
+                    count = 0;
+                }
+            }
+            System.out.println(score);
+        }
+    }
+}
