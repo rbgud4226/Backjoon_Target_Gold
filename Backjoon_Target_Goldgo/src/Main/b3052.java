3baae7aa60a07a84f6bf47d3310aa313c08aa78c
dofla <dofla@192.168.10.101>
2025-01-31 13:48:39 +0900

    "백준 3052 레벨 브론즈2"

---
+++ Backjoon_Target_Goldgo/src/Main/b3052.java
@@ -0,0 +1,26 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class b3052 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> hs = new HashSet<>();
+		String strs = "";
+		int countnum = 0;
+		while(countnum < 10) {
+			strs = br.readLine();
+			int num = Integer.parseInt(strs);
+			int resultnum = num % 42; //나눈 결과값 
+			hs.add(resultnum);
+			countnum++;
+		}
+		System.out.print(hs.size());
+	}
+
+}
