a2913f09365919db16b74783737de6d14285da84
hw <hw@DESKTOP-T06KKGS>
2025-02-03 12:48:32 +0900

    "백준 2292 레벨 브론즈2"

---
+++ Backjoon_Target_Goldgo/src/Main/b2292.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b2292 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		// 1개 6개 12개 18개 24개
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int count = 1; 
+		int start = 2; 
+
+		if (num == 1) {
+			System.out.println(1);
+		} else {
+			while (start <= num) {
+				start = start + (6 * count);
+				count++;
+			}
+			System.out.println(count);
+		}
+	}
+}
