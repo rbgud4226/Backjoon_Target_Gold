b09844391bf321f1d010cbd78d9b4b62d60f84dd
hw <hw@DESKTOP-T06KKGS>
2025-02-05 16:04:09 +0900

    "백준 11650 레벨 실버5(이차원배열 정렬)"

---
+++ Backjoon_Target_Goldgo/src/Main/b11650.java
@@ -0,0 +1,35 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b11650 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int[][] arr = new int[num][2];
+		for (int i = 0; i < num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		Arrays.sort(arr, (a, b) -> {
+			if (a[0] - b[0] == 0) {
+				return a[1] - b[1];
+			} else {
+				return a[0] - b[0];
+			}
+		});
+
+		for (int j = 0; j < arr.length; j++) {
+			System.out.println(arr[j][0] + " " + arr[j][1]);
+		}
+	}
+
+}
