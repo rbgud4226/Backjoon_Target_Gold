0ad1cfb41a19dd2d8ad8f953597c7b0edfe2ce72
hw <hw@DESKTOP-T06KKGS>
2025-02-06 12:03:12 +0900

    "백준 1181 레벨 실버5(정렬 람다식)"

---
+++ Backjoon_Target_Goldgo/src/Main/b1181.java
@@ -0,0 +1,29 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class b1181 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		HashSet<String> hs = new HashSet<>();
+		for(int i=0; i<num; i++) {
+			hs.add(br.readLine());
+		}		
+		List<String> hslist = new ArrayList<>(hs);
+		Collections.sort(hslist);
+		Collections.sort(hslist, (o1,o2) -> o1.length() - o2.length());
+		
+		for(String s : hslist) {
+			System.out.println(s);
+		}
+	}
+}
