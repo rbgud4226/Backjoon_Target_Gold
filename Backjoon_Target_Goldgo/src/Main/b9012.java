c9ff16a09bbdc3e030b803901bac155103ad13d1
hw <hw@DESKTOP-T06KKGS>
2025-02-07 10:09:25 +0900

    "백준 9012 레벨 실버4(괄호 짝맞추기)"

---
+++ Backjoon_Target_Goldgo/src/Main/b9012.java
@@ -0,0 +1,43 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b9012 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		String[] str = new String[num];
+		for (int i = 0; i < num; i++) {
+			String s = br.readLine();
+			int check = 0;
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == '(') {
+					check++;
+				}
+				if (s.charAt(j) == ')') {
+					check--;
+					if (check < 0) { // 닫는 괄호가 여는거보다 먼저나올때
+						check = -1;
+						break;
+					}
+				}
+			}
+
+			if (check == 0) {
+				str[i] = "YES";
+			} else {
+				str[i] = "NO";
+			}
+		}
+
+		for (String s : str) {
+			System.out.println(s);
+		}
+
+	}
+
+}
