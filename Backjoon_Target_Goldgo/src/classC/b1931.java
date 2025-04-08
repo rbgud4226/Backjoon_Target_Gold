93091f324d91f15bbb6d7d11ff9bc974a79052b1
hw <hw@DESKTOP-T06KKGS>
2025-04-08 18:47:49 +0900

    "백준 1931 레벨 실버5(

---
+++ Backjoon_Target_Goldgo/src/classC/b1931.java
@@ -0,0 +1,47 @@
+package classC;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class b1931 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); // 전체 입력 개수
+		int[][] list = new int[n][2];
+		StringTokenizer st;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			list[i][0] = Integer.parseInt(st.nextToken()); // start
+			list[i][1] = Integer.parseInt(st.nextToken()); // end 저장
+		}
+
+		Arrays.sort(list, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
+			}
+		});
+		
+		int count = 0;
+		int endtime = 0;
+		for (int i = 0; i < n; i++) {
+			if (endtime <= list[i][0]) {
+				endtime = list[i][1];
+				count++;
+			}
+		}
+
+		System.out.println(count);
+
+	}
+
+}
